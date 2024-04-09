0783a333d35383eebc3e4be2ad309edad1685be3
TrixDiaz <john.darlucio022@gmail.com>
2024-04-10 00:40:26 +0800

    add landing page content

--- src/components/Jumbotron.tsx
+++ src/components/Jumbotron.tsx
@@ -8,9 +8,9 @@ export const Jumbotron = () => {
 
             <section>
                 <div className="px-4 mx-auto max-w-screen-xl text-center py-24 lg:py-56">
-                    <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-white md:text-5xl lg:text-6xl">We
+                    <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none dark:text-white md:text-5xl lg:text-6xl">We
                         invest in the world’s potential</h1>
-                    <p className="mb-8 text-lg font-normal text-gray-300 lg:text-xl sm:px-16 lg:px-48">Here at Tailwind
+                    <p className="mb-8 text-lg font-normal dark:text-gray-300 lg:text-xl sm:px-16 lg:px-48">Here at Tailwind
                         we focus on markets where technology, innovation, and capital can unlock long-term value and
                         drive economic growth.</p>
                         <Button className={'mr-4'}>Get Started</Button>
